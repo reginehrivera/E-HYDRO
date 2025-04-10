d379e9b8b46d7b5a657f5171616a46e684b22615
sandrineacibar <sandrineacibar19@gmail.com>
2025-04-10 15:19:56 +0800

    background changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,33 @@
 <template>
-  <div class="bg-background-home">
-    <h1>Welcome to Home View</h1>
-    <p>
-      WELCOME TO E-HYDRO SITE .... Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-      laudantium eos doloremque vitae ducimus. Asperiores sit dolores dolore assumenda veniam, nihil
-      saepe nisi aspernatur quia esse nam consequatur amet recusandae!
-    </p>
-  </div>
+  <!--Top Navigatio-->
+ gfj
+  <!--End Top Navigation-->
 </template>
 
 <script>
 /*export default {
   name: 'HomeView',
 }*/
+
+//side bar
+/*export default {
+  data() {
+    return {
+      sidebarVisible: false
+    };
+  },
+  methods: {
+    showSidebar() {
+      this.sidebarVisible = true;
+    },
+    hideSidebar() {
+      this.sidebarVisible = false;
+    }
+  }
+};*/
 </script>
 
 <style scoped>
-.bg-background-home {
-  background-image: url('@/assets/images/bg-home.png') !important;
-  background-size: cover;
-  background-position: center;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+/*Nav Top*/
+
 </style>
