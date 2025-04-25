8a550126a65c2c201f5ee2e9f24b33e94ef15d1f
sandrineacibar <sandrineacibar19@gmail.com>
2025-04-25 11:10:16 +0800

    fix: AquasisView: from developer branch file copied

--- src/views/system/AquasisView.vue
+++ src/views/system/AquasisView.vue
@@ -21,13 +21,13 @@
                 <v-container class="left-container">
                   <v-row class="img-row">
                     <v-col cols="12" class="img-style d-flex">
-                    <img
-                      src="@/assets/img/aquasis-img.jpg"
-                      height="250"
-                      width="380"
-                      class="responsive-img mx-auto"
-                      alt="Aquasis image"
-                    />
+                      <img
+                        src="@/assets/img/aquasis-img.jpg"
+                        height="250"
+                        width="380"
+                        class="responsive-img mx-auto"
+                        alt="Aquasis image"
+                      />
                     </v-col>
                   </v-row>
                   <!--Description-->
@@ -59,7 +59,9 @@
                                 prepend-icon="mdi-facebook-messenger"
                               >
                                 <span class="text-center">Messenger</span>
-                                <v-tooltip activator="parent" location="bottom">Madam Bertud</v-tooltip>
+                                <v-tooltip activator="parent" location="bottom"
+                                  >Madam Bertud</v-tooltip
+                                >
                               </v-btn>
                             </router-link>
                           </v-col>
@@ -72,7 +74,9 @@
                                 prepend-icon="mdi-phone"
                               >
                                 <span class="text-center">Phone Number</span>
-                                <v-tooltip activator="parent" location="bottom">09517978870</v-tooltip>
+                                <v-tooltip activator="parent" location="bottom"
+                                  >09517978870</v-tooltip
+                                >
                               </v-btn>
                             </router-link>
                           </v-col>
@@ -120,31 +124,46 @@
                   </v-row>
                   <v-divider></v-divider>
                   <v-row class="second-row">
-                    <v-col cols="12" md="2" sm="2"><!--1st row-->
+                    <v-col cols="12" md="2" sm="2"
+                      ><!--1st row-->
                       <v-card class="text-center card-img" width="95"
-                        ><img src="@/assets/img/gallon-aquasis.png"
+                        ><img src="@/assets/img/gallon-aquabon.png"
                       /></v-card>
                     </v-col>
-                    <v-col cols="12" md="10" sm="10" class="pl-6"><!--second row-->
-                      <v-row><!--1st row for 2nd row-->
+                    <v-col cols="12" md="10" sm="10" class="pl-6"
+                      ><!--second row-->
+                      <v-row
+                        ><!--1st row for 2nd row-->
                         <v-col cols="12">
                           <h5 class="blue-color">Aquasis Water Refilling Station</h5>
                           <h6>Location: 177 Cancer St., Montilla Blvd Highway, Brgy JP Rizal</h6>
                         </v-col>
                       </v-row>
-                      <v-row><!--2nd row for 2nd row-->
-                        <v-col cols="12" md="6" sm="6" xs="12" class="">
-                          <p>₱ 25.00 per gallon</p>
+                      <v-row
+                        ><!--2nd row for 2nd row-->
+                        <v-col cols="12" md="6" sm="6" xs="12" class="text-h6">
+                          <p>₱ 20.00 per gallon</p>
                         </v-col>
                         <v-col cols="12" md="6" sm="6" xs="12" class="">
-                          <v-container d-flex align-center class="btn-group   ">
+                          <v-container d-flex align-center class="btn-group">
                             <v-row>
-                              <v-btn rounded="0" class="btn-minus" variant="plain" size="small" @click="toggleDecrease"
+                              <v-btn
+                                rounded="0"
+                                class="btn-minus"
+                                variant="plain"
+                                size="small"
+                                @click="toggleDecrease"
                                 ><v-icon>mdi-minus</v-icon></v-btn
                               >
                               <p class="text-h6 text-center">{{ numberOfGallon }}</p>
-                              <v-btn rounded="0" class="btn-plus" variant="plain" size="small" @click="toggleIncrease"
-                                ><v-icon>mdi-plus</v-icon></v-btn>
+                              <v-btn
+                                rounded="0"
+                                class="btn-plus"
+                                variant="plain"
+                                size="small"
+                                @click="toggleIncrease"
+                                ><v-icon>mdi-plus</v-icon></v-btn
+                              >
                             </v-row>
                           </v-container>
                         </v-col>
@@ -154,24 +173,34 @@
                   <v-divider></v-divider>
                   <div class="text-center">
                     <v-btn rounded="0" class="add-address-btn mt-4 mb-4"
-                      ><v-icon class="pr-3 text-center text-primary" size="large">mdi-plus-circle-outline</v-icon><span class="text-dark">Place a new order for another
-                      location</span> </v-btn
-                    >
+                      ><v-icon class="pr-3 text-center text-primary" size="large"
+                        >mdi-plus-circle-outline</v-icon
+                      ><span class="text-dark">Place a new order for another location</span>
+                    </v-btn>
                   </div>
-                  <div class="total-right mb-3"><h4>Total: ₱ {{}}.00</h4></div>
+
+                  <!-- Total -->
+                  <div class="total-right mb-3">
+                    <p>Subtotal: ₱{{ subtotal }}.00</p>
+                    <p v-if="isBulkOrder" class="discount-text">Discount: -₱{{ discount }}.00</p>
+                    <h4>Total: ₱{{ totalPriceWithDiscount }}.00</h4>
+                  </div>
+
                   <v-row class="cancel-order-btns">
-                      <v-col cols="12" md="6" class="text-start ">
-                        <v-btn variant="none" class="cancel-btn">Cancel</v-btn>
-                      </v-col>
-                      <v-col cols="12" md="6" class="">
+                    <v-col cols="12" md="6" class="text-start">
+                      <v-btn variant="none" class="cancel-btn">Cancel</v-btn>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                      <router-link class="no-underline" to="/summary">
                         <v-btn variant="none" class="order-btn">Order Now</v-btn>
-                      </v-col>
+                      </router-link>
+                    </v-col>
                   </v-row>
                 </v-container>
                 <v-container>
                   <v-row class="text-center mx-auto">
                     <v-col cols="12" md="4" sm="4" xs="4" class="set-sched-btn">
-                      <router-link class="no-underline" to="#">
+                      <router-link class="no-underline" to="/calendar">
                         <v-btn variant="none" class="full-btn">
                           <div>
                             <h3 class="set"><v-icon>mdi-calendar</v-icon> Set a delivery</h3>
@@ -182,7 +211,7 @@
                     </v-col>
 
                     <v-col cols="12" md="4" sm="4" xs="4" class="bulk-btn">
-                      <router-link class="no-underline" to="#" >
+                      <router-link class="no-underline" to="#" @click="orderInBulk">
                         <v-btn variant="none" class="full-btn">
                           <div>
                             <h5>Order in bulk</h5>
@@ -193,7 +222,7 @@
                     </v-col>
 
                     <v-col cols="12" md="4" sm="4" xs="4" class="order-bottom-btn">
-                      <router-link class="no-underline" to="#">
+                      <router-link class="no-underline" to="/summary">
                         <v-btn variant="none" class="full-btn">
                           <div>
                             <h5>Order now</h5>
@@ -221,24 +250,44 @@ import StationLayout from '@/components/layout/StationLayout.vue'
 import NavigationBar from '@/components/layout/NavigationBar.vue'
 import '@/assets/main.css'
 
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const selected = ref([''])
 const items = ['']
 
 // Increasing and Decreasing number of gallon
 const numberOfGallon = ref(0)
+const isBulkOrder = ref(false)
+
 const toggleIncrease = () => {
   numberOfGallon.value++
+  isBulkOrder.value = numberOfGallon.value >= 12
 }
+
 const toggleDecrease = () => {
   if (numberOfGallon.value > 0) {
     numberOfGallon.value--
+    isBulkOrder.value = numberOfGallon.value >= 12
   }
 }
+
+// Method for bulk order
+const orderInBulk = () => {
+  numberOfGallon.value = 12
+  isBulkOrder.value = true
+}
+
+// Computed property for calculating total price
+const pricePerGallon = 20
+const subtotal = computed(() => numberOfGallon.value * pricePerGallon)
+const discount = computed(() => (isBulkOrder.value ? 10 : 0))
+const totalPriceWithDiscount = computed(() => subtotal.value - discount.value)
 </script>
 
-
-<style>
+<style scoped>
+.discount-text {
+  color: red;
+  font-weight: normal;
+  font-size: 15px;
+}
 </style>
-
